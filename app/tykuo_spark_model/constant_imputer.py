6ad1e2140652bf3dcf10fd379e21c7cbcb005b6e
b96705008 <b96705008@gmail.com>
2018-04-23 15:32:18 +0800

    refactor constant val impute

--- app/tykuo_spark_model/constant_imputer.py
+++ app/tykuo_spark_model/constant_imputer.py
@@ -9,8 +9,17 @@ from pyspark.ml.param.shared import *
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
 from params import HasConstValue
 
-class ConstantImputer(Transformer, HasConstValue, DefaultParamsReadable, DefaultParamsWritable):
+
+class ConstantImputer(Transformer, HasInputCols, HasConstValue, 
+    DefaultParamsReadable, DefaultParamsWritable):
     
     def _transform(self, dataset):
         const_val = self.getConstValue()
-        return dataset.na.fill(const_val)
+        try:
+            xs = self.getInputCols()
+            if type(const_val) is dict:
+                raise Exception('Multiple fields can only be filled with single value.')
+            val_dict = {x: const_val for x in xs}
+            return dataset.na.fill(val_dict)
+        except KeyError as e:
+            return dataset.na.fill(const_val)
